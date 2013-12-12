817d6dbb1c69e608271ae49d7b2e21d1f2c24900
nick-donald <nickjdonald@gmail.com>
2013-12-11 22:20:03 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rowtrack::Application.config.secret_key_base = 'e87665163ee91a5018bdec86eae4f4b0a1c3810d85ebe24df5d4436bab08a08263101d119a53acaa7e345cb5b60c9ec27e4ce1d64aca8bc6925dbdc7f1c70a55'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rowtrack::Application.config.session_store :cookie_store, key: '_rowtrack_session'
